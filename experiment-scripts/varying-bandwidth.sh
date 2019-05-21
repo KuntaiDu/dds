23229d02f63666983ec73bf005b161df93cc2c63
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-05-21 10:08:57 -0500

    Add more low and high threshold ranges to try out

--- experiment-scripts/varying-bandwidth.sh
+++ experiment-scripts/varying-bandwidth.sh
@@ -22,10 +22,49 @@ do
        mkdir results
     fi
 
-    for threshold in {0.1,0.2,0.3,0.4,0.5,0.6,0.7};
+    for low_threshold in {0.3, 0.35, 0.4, 0.45};
     do
-        python play_video.py --vid-name results/${vidname} --output-file stats --src ${images_dir} \
-               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt ${threshold} -ht 0.8 --verbosity info
+        for threshold in {0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+        do
+            python play_video.py --vid-name results/${vidname}_${low_threshold}_${threshold} --output-file stats --src ${images_dir} \
+                   --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+                   -lt ${low_threshold} -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+        done
     done
+
+    for threshold in {0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    do
+        python play_video.py --vid-name results/${vidname}_0.5_${threshold} --output-file stats --src ${images_dir} \
+               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+               -lt 0.5 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+    done
+
+    for threshold in {0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    do
+        python play_video.py --vid-name results/${vidname}_0.55_${$threshold} --output-file stats --src ${images_dir} \
+               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+               -lt 0.55 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+    done
+
+    for threshold in {0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    do
+        python play_video.py --vid-name results/${vidname}_0.60_${$threshold} --output-file stats --src ${images_dir} \
+               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+               -lt 0.60 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+    done
+
+    for threshold in {0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    do
+        python play_video.py --vid-name results/${vidname}_0.65_${$threshold} --output-file stats --src ${images_dir} \
+               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+               -lt 0.65 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+    done
+
+    for threshold in {0.75, 0.8, 0.85, 0.9, 0.95};
+    do
+        python play_video.py --vid-name results/${vidname}_0.70_${$threshold} --output-file stats --src ${images_dir} \
+               --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
+               -lt 0.70 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+    done
+
 done
