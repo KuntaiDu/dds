79f461723c9382902cc873110d9c3b1ed17ce041
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-05-21 11:27:03 -0500

    Fix bugs due to blank spaces in varying-bandwidth.sh

--- experiment-scripts/varying-bandwidth.sh
+++ experiment-scripts/varying-bandwidth.sh
@@ -22,55 +22,55 @@ do
        mkdir results
     fi
 
-    for low_threshold in {0.3, 0.35, 0.4, 0.45};
+    for low_threshold in {0.3,0.35,0.4,0.45};
     do
-        for threshold in {0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+        for threshold in {0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95};
         do
             echo "Running ${vidname} -- ${low_threshold} ${threshold}"
             python play_video.py --vid-name results/${vidname}_${low_threshold}_${threshold} --output-file stats --src ${images_dir} \
                    --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-                   -lt ${low_threshold} -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+                   --low-threshold ${low_threshold} --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
         done
     done
 
-    for threshold in {0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    for threshold in {0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95};
     do
         echo "Running 0.5 -- ${low_threshold} ${threshold}"
         python play_video.py --vid-name results/${vidname}_0.5_${threshold} --output-file stats --src ${images_dir} \
                --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt 0.5 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+               --low-threshold 0.5 --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
     done
 
-    for threshold in {0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    for threshold in {0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95};
     do
         echo "Running 0.55 -- ${low_threshold} ${threshold}"
         python play_video.py --vid-name results/${vidname}_0.55_${$threshold} --output-file stats --src ${images_dir} \
                --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt 0.55 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+               --low-threshold 0.55 --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
     done
 
-    for threshold in {0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    for threshold in {0.65,0.7,0.75,0.8,0.85,0.9,0.95};
     do
         echo "Running 0.60 -- ${low_threshold} ${threshold}"
         python play_video.py --vid-name results/${vidname}_0.60_${$threshold} --output-file stats --src ${images_dir} \
                --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt 0.60 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+               --low-threshold 0.60 --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
     done
 
-    for threshold in {0.7, 0.75, 0.8, 0.85, 0.9, 0.95};
+    for threshold in {0.7,0.75,0.8,0.85,0.9,0.95};
     do
         echo "Running 0.65 -- ${low_threshold} ${threshold}"
         python play_video.py --vid-name results/${vidname}_0.65_${$threshold} --output-file stats --src ${images_dir} \
                --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt 0.65 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+               --low-threshold 0.65 --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
     done
 
-    for threshold in {0.75, 0.8, 0.85, 0.9, 0.95};
+    for threshold in {0.75,0.8,0.85,0.9,0.95};
     do
         echo "Running 0.70 -- ${low_threshold} ${threshold}"
         python play_video.py --vid-name results/${vidname}_0.70_${$threshold} --output-file stats --src ${images_dir} \
                --low-results ${low_path} --high-results ${high_path} --ground-truth ${groundtruth} \
-               -lt 0.70 -ht ${threshold} --intersection-threshold 0.3 --verbosity info
+               --low-threshold 0.70 --high-threshold ${threshold} --intersection-threshold 0.3 --verbosity info
     done
 
 done
