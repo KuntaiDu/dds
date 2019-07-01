d08f20fc146521af6f76dd6de9e1d71f068b2897
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-06-30 21:08:40 -0500

    Fix some basic mistakes in all-videos

--- experiment-scripts/all-videos.sh
+++ experiment-scripts/all-videos.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DATASET_ORIGIN=../scratch
+DATASET_ORIGIN=../new_dataset
 OUTPUT_FILE=stats
 
 if [ ! -d "results" ];
@@ -11,19 +11,19 @@ fi
 echo "Running Groundtruth and MPEG"
 for vid_path in $DATASET_ORIGIN/*
 do
-    vid_name=$(echo ${vid_path} | cut -f2 -d/)
+    vid_name=$(echo ${vid_path} | cut -f3 -d/)
+    original_images=${vid_path}/src/
 
     # Ground truth
-    original_images=${vid_path}/src/
-    python play_video.py --vid-name results/${vidname}_gt --low-src ${original_images} --high-src ${original_images} --resolutions 1.0 \
-           --output-file ${OUTPUT_FILE} --max-size 0.8 --low-threshold 0.3 --high-threshold 0.8 --verbosity info
+    python play_video.py --vid-name results/${vid_name}_gt --low-src ${original_images} --high-src ${original_images} --resolutions 1.0 \
+           --output-file ${OUTPUT_FILE} --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --verbosity info
 
     # Run MPEG
-    for resolution in {0.1,0.15,0.2,0.25,0.375,0.5,0.625,0.75,0.875};
+    for res in {0.15,0.2,0.25,0.375,0.5,0.625,0.75};
     do
-        images_direc=${vid_path}/src/${resolution}/
-        python play_video.py --vid-name results/${vidname}_DDS_${resolution} --low-src ${images_direc} --resolutions ${resolution} \
-               --high-src ${original_images} --output-file ${OUTPUT_FILE} --ground-truth results/${vidname}_gt --max-size 0.8 \
+        images_direc=${vid_path}/src/${res}/
+        python play_video.py --vid-name results/${vid_name}_mpeg_${res} --low-src ${images_direc} --resolutions ${res} \
+               --high-src ${original_images} --output-file ${OUTPUT_FILE} --ground-truth results/${vid_name}_gt --max-size 0.3 \
                --low-threshold 0.3 --high-threshold 0.8 --verbosity info
     done
 
@@ -32,15 +32,16 @@ done
 echo "Running DDS"
 for vid_path in $DATASET_ORIGIN/*
 do
-    vid_name=$(echo ${vid_path} | cut -f2 -d/)
+    vid_name=$(echo ${vid_path} | cut -f3 -d/)
     original_images=${vid_path}/src/
     # Run DDS
     low=0.2
     for high in {0.375,0.5};
     do
-        low_images=${vid_path}/src/${low}/
-        python play_video.py --vid-name results/${vidname}_DDS_${resolution} --low-src ${low_images} --high-src ${original_images} \
-               --resolutions ${low} ${high} --output-file ${OUTPUT_FILE} --ground-truth results/${vidname}_gt --max-size 1.0 \
-               --low-threshold 0.3 --high-threshold 0.8 --verbosity info
+        low_images=${vid_path}/src/${low}
+        low_results=results/${vid_name}_mpeg_${low}
+        python play_video.py --vid-name results/${vid_name}_dds_${low}_${high} --low-src ${low_images} --high-src ${original_images} \
+               --resolutions ${low} ${high} --low-results ${low_results} --output-file ${OUTPUT_FILE} --ground-truth results/${vid_name}_gt \
+               --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --verbosity info
     done
 done
