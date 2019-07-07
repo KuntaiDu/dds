5e988d6b5bfeaf0676dd4efba7ced0aef0609c90
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-07-07 15:47:35 -0500

    Prepare the data needed for DDS

---
+++ experiment-scripts/prepare_data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DATASET_ORIGIN=$1
+
+ROOT=`pwd`/..
+SRC=${ROOT}/new_dataset
+
+echo $ROOT
+
+if [ ! -d ${SRC} ];
+then
+    mkdir ${SRC}
+fi
+
+for vid_path in $DATASET_ORIGIN/*
+do
+    vid_name=$(echo ${vid_path} | cut -f5 -d/ | cut -f1 -d.)
+    echo "====================Running ${vid_name}===================="
+    if [ ${vid_name} != "drivers_0_00_00_00" ];
+    then
+        continue
+    fi
+
+
+    vid_src_path=${SRC}/${vid_name}
+
+    if [ ! -d ${vid_src_path} ];
+    then
+        mkdir -p ${vid_src_path}/src
+        cp ${vid_path} ${vid_src_path}/src/
+        ffmpeg -y -i ${vid_src_path}/src/${vid_name}.mp4 -pix_fmt yuvj420p -vsync 0 -start_number 0 ${vid_src_path}/src/%010d.png
+    fi
+
+done
