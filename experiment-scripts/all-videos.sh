46b25c7f6bfeeb0a10a8f977c0ee01a47f5f45f2
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-07-08 12:06:58 -0500

    Add enforce-iframe flag to all DDS and MPEG runs

--- experiment-scripts/all-videos.sh
+++ experiment-scripts/all-videos.sh
@@ -16,7 +16,7 @@ do
 
     # Ground truth
     python play_video.py --vid-name results/${vid_name}_gt --low-src ${original_images} --high-src ${original_images} --resolutions 1.0 \
-           --output-file ${OUTPUT_FILE} --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --verbosity info
+           --output-file ${OUTPUT_FILE} --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --enforce-iframes --verbosity info
 
     # Run MPEG
     for res in {0.15,0.2,0.25,0.375,0.5,0.625,0.75};
@@ -24,7 +24,7 @@ do
         images_direc=${vid_path}/src/${res}/
         python play_video.py --vid-name results/${vid_name}_mpeg_${res} --low-src ${images_direc} --resolutions ${res} \
                --high-src ${original_images} --output-file ${OUTPUT_FILE} --ground-truth results/${vid_name}_gt --max-size 0.3 \
-               --low-threshold 0.3 --high-threshold 0.8 --verbosity info
+               --low-threshold 0.3 --high-threshold 0.8 --enforce-iframes --verbosity info
     done
 
 done
@@ -42,6 +42,6 @@ do
         low_results=results/${vid_name}_mpeg_${low}
         python play_video.py --vid-name results/${vid_name}_dds_${low}_${high} --low-src ${low_images} --high-src ${original_images} \
                --resolutions ${low} ${high} --low-results ${low_results} --output-file ${OUTPUT_FILE} --ground-truth results/${vid_name}_gt \
-               --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --verbosity info
+               --max-size 0.3 --low-threshold 0.3 --high-threshold 0.8 --enforce-iframes --verbosity info
     done
 done
