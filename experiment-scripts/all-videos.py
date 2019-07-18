95e40a846d75e7e9781715ecc36233af8996bb25
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-07-17 21:57:08 -0500

    Add new videos and their qp values

--- experiment-scripts/all-videos.py
+++ experiment-scripts/all-videos.py
@@ -7,8 +7,24 @@ gt_qps = {
     "highway_1_24_00_00": 28,
     "interchange_0_00_00_00": 25,
     "interchange_1_00_00_00": 25,
+    "flyover_4_00_00_00": 32,
+    "highway_1_45_00_00": 31,
+    "street_0_00_00_07": 30,
+    "interchange_0_00_00_1": 30,
+    "highway_1_47_00_00": 31,
+    "ladash_002_00_00_00": 34,
+    "ladash_000_00_00_00": 33
 }
 mpeg_resolutions = [0.15, 0.2, 0.25, 0.375, 0.5, 0.625, 0.75]
+mpeg_qps = {
+    "flyover_4_00_00_00": [35, 36, 37, 38, 39, 40],
+    "highway_1_45_00_00": [33, 35, 36, 37, 38, 39, 40],
+    "street_0_00_00_07": [32, 35, 36, 37, 38, 39, 40],
+    "interchange_0_00_00_1": [32, 35, 36, 37, 38, 39, 40],
+    "highway_1_47_00_00": [33, 35, 36, 37, 38, 39, 40],
+    "ladash_002_00_00_00": [35, 36, 37, 38, 39, 40],
+    "ladash_000_00_00_00": [35, 36, 37, 38, 39, 40]
+}
 dds_config = {
     "flyover_1_00_00_00": ([(0.375, 0.75, 39, 35), (0.375, 0.75, 39, 39)]),
     "highway_1_24_00_00": ([(0.25, 0.5, 35, 35), (0.25, 0.75, 35, 35)]),
@@ -27,15 +43,14 @@ for video in vid_names:
     else:
         print(f"Skipping {vname}")
 
-    # # Run MPEG
-    # for res in mpeg_resolutions:
-    #     _, qps = dds_config[video]
-    #     for qp in qps:
-    #         vname = f"{video}_mpeg_{res}_{qp}"
-    #         if not os.path.exists(os.path.join("results", vname)):
-    #             os.system(f"bash run_single.sh mpeg {video} {vname} {qp} {res}")
-    #         else:
-    #             print(f"Skipping {vname}")
+    # Run MPEG
+    for res in mpeg_resolutions:
+        for qp in mpeg_qps[video]:
+            vname = f"{video}_mpeg_{res}_{qp}"
+            if not os.path.exists(os.path.join("results", vname)):
+                os.system(f"bash run_single.sh mpeg {video} {vname} {qp} {res}")
+            else:
+                print(f"Skipping {vname}")
 
     # Run DDS
     for low_res, high_res, low_qp, high_qp in dds_config[video]:
