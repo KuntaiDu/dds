6d3d7dd7f5b9fd175615b2f3d135fc4253b080bb
Xin Yuan <2608174246@qq.com>
2019-08-30 19:08:21 -0500

    merge rpn to obj_detector

--- backend/object_detector.py
+++ backend/object_detector.py
@@ -39,6 +39,8 @@ class Detector:
             ops = tf.compat.v1.get_default_graph().get_operations()
             all_tensor_names = {output.name for op in ops
                                 for output in op.outputs}
+
+            # FOR RCNN final layer results:
             tensor_dict = {}
             for key in [
                     'num_detections', 'detection_boxes',
@@ -50,6 +52,18 @@ class Detector:
                     tensor_dict[key] = (tf.compat.v1.get_default_graph()
                                         .get_tensor_by_name(tensor_name))
 
+            # FOR RPN intermedia results
+            key_tensor_map = {
+                'RPN_box_no_normalized' : 'BatchMultiClassNonMaxSuppression/map/while/MultiClassNonMaxSuppression/Gather/Gather:0',
+                'RPN_score' : 'BatchMultiClassNonMaxSuppression/map/while/MultiClassNonMaxSuppression/Gather/Gather_2:0',
+                'Resized_shape' : 'Preprocessor/map/while/ResizeToRange/stack_1:0',
+            }
+
+            for key, tensor_name in key_tensor_map.items():
+                if tensor_name in all_tensor_names:
+                    tensor_dict[tensor_name] = (tf.compat.v1.get_default_graph()
+                                        .get_tensor_by_name(tensor_name))
+
             image_tensor = (tf.compat.v1.get_default_graph()
                             .get_tensor_by_name('image_tensor:0'))
             # Run inference
@@ -57,6 +71,14 @@ class Detector:
             output_dict = self.session.run(tensor_dict,
                                            feed_dict=feed_dict)
 
+            # FOR RPN intermedia results
+            w = output_dict[key_tensor_map['Resized_shape']][1]
+            h = output_dict[key_tensor_map['Resized_shape']][0]
+            input_shape_array = np.array([h, w, h, w])
+            output_dict['RPN_box_normalized'] = output_dict[key_tensor_map['RPN_box_no_normalized']]/input_shape_array[np.newaxis,:]
+            output_dict['RPN_score'] = output_dict[key_tensor_map['RPN_score']]
+
+            # FOR RCNN final layer results
             # all outputs are float32 numpy arrays,
             # so convert types as appropriate
             output_dict['num_detections'] = int(
@@ -71,6 +93,8 @@ class Detector:
 
     def infer(self, image_np):
         imgae_crops = image_np
+
+        # this output_dict contains both final layer results and RPN results
         output_dict = self.run_inference_for_single_image(
             imgae_crops, self.d_graph)
 
@@ -92,4 +116,31 @@ class Detector:
             box_tuple = (xmin, ymin, xmax - xmin, ymax - ymin)
             results.append((object_class, confidence, box_tuple))
 
-        return results
+
+        # threshold hard coding here
+        threshold_RPN = 0.5
+
+        results_only_RPN = []
+        # results_RPN_RCNN contains only RPN results, NO low_config results!
+        # low_config results is in Variable: results
+
+        frame_with_no_results = True
+        for idx_region, region in enumerate(output_dict['RPN_box_normalized']):
+            x = region[1]
+            y = region[0]
+            w = region[3] - region[1]
+            h = region[2] - region[0]
+            conf = output_dict['RPN_score'][idx_region]
+            if conf < threshold_RPN or w*h == 0. or w*h > 0.04:
+                continue
+            # print((idx, idx_region))
+            single_region = Region(idx, x, y, w, h, conf, 'object',
+                                   scale, 'generic')
+            RPN_Regions.append(single_region)
+            frame_with_no_results = False
+
+        if frame_with_no_results:
+            RPN_Regions.append(
+                Region(idx, 0, 0, 0, 0, 0.1, "no obj", scale))
+
+        return results, results_only_RPN
