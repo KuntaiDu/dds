e5068d3f45e37d59fba20e5401f09a4f3e52f13f
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-09-03 15:42:59 -0500

    Change experiment scripts to fit new commandline arguments

--- experiment-scripts/all-videos.py
+++ experiment-scripts/all-videos.py
@@ -40,34 +40,18 @@ mpeg_qps = {
     "twoway_0_00_00_00": [29, 33, 35, 37, 38, 39, 40]
 }
 dds_config = {
-    "manhattan_021_00_00_00": [(0.2, 39, 0.25, 35), (0.2, 36, 0.625, 38)],
-    "flyover_4_00_00_00": [(0.25, 40, 0.5, 40), (0.25, 39, 0.5, 40)],
-    "flyover_2_00_00_00": [(0.25, 40, 0.375, 40), (0.375, 40, 0.625, 38)],
-    "highway_1_00_00_00": [(0.25, 40, 0.5, 40)],
-    "highway_0_00_00_00": [(0.375, 40, 0.75, 36)],
-    "out10fps720p": [(0.5, 40, 0.75, 36), (0.375, 40, 0.75, 36), (0.375, 40, 0.9, 32), (0.5, 40, 0.9, 32), (0.5, 40, 0.8, 32), (0.5, 40, 0.9, 36)],
-    "ladash_002_00_00_00": [(0.25, 40, 0.5, 40), (0.25, 39, 0.5, 40)],
-    "ladash_000_00_00_00": [(0.25, 39, 0.625, 40), (0.25, 38, 0.625, 40)],
-    "street_0_00_00_07": [(0.375, 40, 0.625, 40), (0.25, 39, 0.625, 40)],
-    "flyover_1_00_00_00": [(0.2, 37, 0.625, 40), (0.25, 38, 0.625, 40), (0.25, 37, 0.625, 40)],
-    "interchange_0_00_00_1": [(0.375, 40, 0.5, 36), (0.25, 39, 0.5, 39)],
-    "interchange_1_00_00_00": [(0.375, 40, 0.5, 38)],
-    "highway_1_24_00_00": [(0.25, 39, 0.5, 40)],
-    "interchange_0_00_00_00": [(0.375, 40, 0.5, 38)],
-    "highway_1_47_00_00": [(0.2, 40, 0.375, 40), (0.25, 40, 0.5, 40)],
-    "twoway_0_00_00_00": []
+    "highway_0_00_00_00": [
+        (0.9, 40, 0.9, 26, 0., 15, 0.5, 0., 0.0025)]
 }
 vid_names = sorted(gt_qps.keys())
 
 for video in vid_names:
-    # Generate gt
-    if video != "drive_sing_cut10fps":
-        continue
+    # Generate GT
     res = 1.0
     qp = gt_qps[video]
     vname = f"{video}_gt"
     if not os.path.exists(os.path.join("results", vname)):
-        os.system(f"bash ./experiment-scripts/run_single.sh gt {video} {vname} {qp}")
+        os.system(f"bash run_single.sh gt {video} {vname} {qp}")
     else:
         print(f"Skipping {vname}")
 
@@ -77,14 +61,21 @@ for video in vid_names:
             print(str(res) + '/' + str(qp))
             vname = f"{video}_mpeg_{res}_{qp}"
             if not os.path.exists(os.path.join("results", vname)):
-                os.system(f"bash ./experiment-scripts/run_single.sh mpeg {video} {vname} {qp} {res}")
+                os.system(f"bash run_single.sh mpeg {video} {vname} {qp} {res}")
             else:
                 print(f"Skipping {vname}")
 
     # Run DDS
-    for low_res, low_qp, high_res, high_qp in dds_config[video]:
-        vname = f"{video}_dds_{low_res}_{high_res}_{low_qp}_{high_qp}"
+    for (low_res, low_qp, high_res, high_qp,
+         rpn_enlarge_ratio, batch_size, prune_score,
+         objfilter_iou, size_obj) in dds_config[video]:
+        vname = (f"{video}_dds_{low_res}_{high_res}_{low_qp}_{high_qp}_"
+                 "{rpn_enlarge_ratio}_twosides_batch_{batch_size}_"
+                 "{prune_score}_{objfilter_iou}_{size_obj}")
         if not os.path.exists(os.path.join("results", vname)):
-            os.system(f"bash ./experiment-scripts/run_single.sh dds {video} {vname} {low_qp} {high_qp} {low_res} {high_res}")
+            os.system(f"bash run_single.sh dds {video} {vname} {low_qp} "
+                      "{high_qp} {low_res} {high_res} {rpn_enlarge_ratio} "
+                      "{batch_size} {prune_score} "
+                      "{objfilter_iou} {size_obj}")
         else:
             print(f"Skipping {vname}")
--- experiment-scripts/run_single.sh
+++ experiment-scripts/run_single.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DATASET_ORIGIN=/data/yuanx/new_dataset
+DATASET_ORIGIN=../new_dataset
 OUTPUT_FILE=stats
 
 if [ ! -d "results" ];
@@ -30,15 +30,23 @@ then
            --max-size 0.3 --low-threshold 0.3 --high-threshold 0.3 --enforce-iframes --qp ${qp} --verbosity info
 elif [ $mode == "dds" ];
 then
-    vid_name=$2
     vname=$3
     low_qp=$4
     high_qp=$5
     low=$6
     high=$7
-    low_results=backend/no_filter_denoise_combined_merged_bboxes/${video}_mpeg_${low}_${low_qp}
-    python play_video.py --vid-name results/${vname} --high-src ${original_images} \
+    rpn_enlarge_ratio=$8
+    batch_size=$9
+    prune_score=$10
+    objfilter_iou=$11
+    size_obj=$12
+    low_results=results/${video}_mpeg_${low}_${low_qp}
+    python play_video.py --vid-name ${RESULTS_DIR}/${vname} --high-src ${original_images} \
            --resolutions ${low} ${high} --low-results ${low_results} --output-file ${OUTPUT_FILE} \
-           --ground-truth results/${video}_mpeg_0.9_30 --max-size 0.3 \
-           --low-threshold 0.3 --high-threshold 0.3 --enforce-iframes --qp ${low_qp} ${high_qp} --verbosity info
+           --ground-truth results/${video}_gt --max-size 0.3 \
+           --low-threshold 0.3 --high-threshold 0.3 --enforce-iframes \
+           --batch-size ${batch_size} --prune-score ${prune_score} \
+           --objfilter-iou ${objfilter_iou} --size-obj ${size_obj}
+           --qp ${low_qp} ${high_qp} --verbosity info --rpn_enlarge_ratio ${rpn_enlarge_ratio} \
+
  fi
