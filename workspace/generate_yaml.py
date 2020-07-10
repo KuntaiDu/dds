024c334b3440b49f72feaf1ae4c0982eb116d8b2
Alex-q-z <qizhengz@uchicago.edu>
2020-07-10 11:44:16 -0500

    Qizheng: fixed parameter sweeping

---
+++ workspace/generate_yaml.py
@@ -0,0 +1,35 @@
+import yaml
+
+configuration = {
+    'data_dir': '/tank/qizheng/new_dataset',
+    'instances': [
+        {
+            'method': 'gt',
+            'video_name': 'trafficcam_1',
+            'qp': 34
+            },
+        {
+            'method': 'mpeg',
+            'video_name': 'trafficcam_1',
+            'resolution': [0.375, 0.5, 0.625, 0.7, 0.75, 0.8, 0.9],
+            'qp': [35, 36, 40]
+            },
+        {
+            'method': 'dds',
+            'video_name': 'trafficcam_1',
+            'low_res': 0.9,
+            'low_qp': 40,
+            'high_res': 0.9,
+            'high_qp': 26,
+            'rpn_enlarge_ratio': 0.,
+            'batch_size': 15,
+            'prune_score': 0.5,
+            'objfilter_iou': 0.,
+            'size_obj': 0.0025
+            }
+    ]
+}
+
+with open('configuration.yml', 'w') as config:
+    documents = yaml.dump(configuration, config)
+    print(documents)
